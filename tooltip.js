f365a46489a394870fc4710256247318c2d64b82
TRINH Thi Thanh Xuan <xuantrinh2287@gmail.com>
2012-05-14 14:53:09 +0200

    The css background-color added

--- tooltip.js
+++ tooltip.js
@@ -11,53 +11,44 @@
 	$.fn.tooltip= function(options){
 		var settings = $.extend( {
 			'color'             : '#fff',
-			'background'		: '#C81E2B',
 			'width'				: '350px'
 		}, options);
+		
 		var tooltip = document.createElement('div');
 		var id='tooltip'+Math.floor(Math.random()*1000);
 		
 		$(this).data('numid',id);
 		$(tooltip).attr('id',id);
 
-		$('a[rel=tooltip]').mouseover(function(e) {
+		$('a[rel=tooltip]').css('display','inline-block').mouseover(function(e) {
 
 			var page = $(this).attr('href');
-
+			
 			$(tooltip).css({
 				'position':'absolute',
 				'padding':'8px 8px 8px 8px',
-				'background': settings.background,
-				'filter':'progid:DXImageTransform.Microsoft.gradient(from(#F04349), to(#C81E2B))', /* pour IE */
-
-				'background': '-webkit-gradient(linear, left top, left bottom, from(#F04349), to(#C81E2B))', /* pour webkit */
-
-				'background': '-moz-linear-gradient(top,  #F04349, #C81E2B)', /* pour firefox 3.6+ */
-			
-				
-				'-moz-box-shadow': '0 1px 3px rgba(0,0,0,0.5)',
+				'background-color': 'rgba(252, 47, 47,0.8)',
 				'-webkit-box-shadow': '0 1px 3px rgba(0,0,0,0.5)',
+				'-moz-box-shadow': '0 1px 3px rgba(0,0,0,0.5)',
 				'text-shadow': '0 -1px 1px rgba(0,0,0,0.25)',
 				'border-bottom': '1px solid rgba(0,0,0,0.25)',
 				
 				'border-radius': '10px',
-				'-moz-border-radius': '5px',
 				'-webkit-border-radius': '5px',
+				'-moz-border-radius': '20px',
 				'color':settings.color,
 				'width':settings.width,
 				'font-size':'15px',
 				'display':'none'
-			}).fadeIn(400);
-
+			}).fadeIn(40);
 			$(this).append(tooltip);
 			
 			$(tooltip).load(page+' div>p');
-			
 		}).mousemove(function(e){
 			$(tooltip).css({'left':e.pageX-700, 'top':e.pageY-100});
-		}).mouseout(function(){
+		}).mouseout(function(e){
 			var id = $(this).data('numid');
-			$("#"+id).fadeOut(400);
+			$("#"+id).fadeOut(40);
 		});
 	};
 
